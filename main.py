107b2ef21320fbfa3018d362ddedddb0a29a7a70
Vidoje Zeljic <vidoje.zeljic@fioneer.com>
2025-01-30 22:56:38 +0100

    format main.py, extend max-line-length to 120

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+
 # ORM model
 class TaskDB(Base):
     __tablename__ = "tasks"
@@ -18,8 +19,10 @@ class TaskDB(Base):
     name = Column(String, index=True, nullable=False)
     description = Column(String, nullable=False)
 
+
 # Create the database tables
 Base.metadata.create_all(bind=engine)
+
 
 # Pydantic model
 class Task(BaseModel):
@@ -30,8 +33,10 @@ class Task(BaseModel):
     class Config:
         from_attributes = True
 
+
 # FastAPI app
 app = FastAPI()
+
 
 # Dependency to get DB session
 def get_db():
@@ -41,10 +46,12 @@ def get_db():
     finally:
         db.close()
 
+
 @app.get("/tasks", response_model=List[Task], status_code=200)
 def get_tasks(db: Session = Depends(get_db)):
     tasks = db.query(TaskDB).all()
     return tasks
+
 
 @app.get("/tasks/{task_id}", response_model=Task, status_code=200)
 def get_task(task_id: int, db: Session = Depends(get_db)):
@@ -52,6 +59,7 @@ def get_task(task_id: int, db: Session = Depends(get_db)):
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
     return task
+
 
 @app.post("/tasks", response_model=Task, status_code=201)
 def create_task(task: Task, db: Session = Depends(get_db)):
@@ -65,6 +73,7 @@ def create_task(task: Task, db: Session = Depends(get_db)):
     db.refresh(new_task)
     return new_task
 
+
 @app.put("/tasks/{task_id}", response_model=Task, status_code=200)
 def update_task(task_id: int, updated_task: Task, db: Session = Depends(get_db)):
     task = db.query(TaskDB).filter(TaskDB.id == task_id).first()
@@ -76,6 +85,7 @@ def update_task(task_id: int, updated_task: Task, db: Session = Depends(get_db))
     db.commit()
     db.refresh(task)
     return task
+
 
 @app.delete("/tasks/{task_id}", status_code=204)
 def delete_task(task_id: int, db: Session = Depends(get_db)):
